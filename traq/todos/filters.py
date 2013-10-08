6be55c473d554e993ead705e0b460f6f7c4682a1
Brandon Conway <conwayb@pdx.edu>
2013-10-07 17:59:11 -0700

    less gross due on filter

--- traq/todos/filters.py
+++ traq/todos/filters.py
@@ -1,15 +1,16 @@
+from django.utils.translation import gettext as _
 import django_filters
 from .models import ToDo  
-from traq.tickets.filters import range_from_today, StartDateRangeFilter, SprintEndRangeFilter, TicketFilterSet
+from traq.tickets.filters import range_from_today, StartDateRangeFilter, TicketFilterSet
 import datetime
 
 class ToDoFilterSet(TicketFilterSet):
     def __init__(self, *args, **kwargs):
             super(ToDoFilterSet, self).__init__(*args, **kwargs)
             self.filters.pop('priority')
-
+            
     class Meta:
         model = ToDo
-        fields = ('status', 'due_range', 'sprint_end')
+        fields = ('status', 'due_range','sprint_end')
 
 
