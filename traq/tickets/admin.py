b6bdcf24c934b36b5b3d8258488cf75675020ff9
Brandon Conway <conwayb@pdx.edu>
2014-06-26 10:14:23 -0700

    exposed worktype to admin for easy hourly rate change

--- traq/tickets/admin.py
+++ traq/tickets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin    
-from traq.tickets.models import Ticket, TicketType, TicketStatus
+from traq.tickets.models import Ticket, TicketType, TicketStatus, WorkType
 
 admin.site.register(TicketType)
 admin.site.register(TicketStatus)
+admin.site.register(WorkType)
