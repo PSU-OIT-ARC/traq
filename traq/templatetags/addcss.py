76fcd407ca73762e22e8c246af113dd2aa7a7a54
Brandon Conway <conwayb@pdx.edu>
2014-07-17 17:06:52 -0700

    new bootstrap form tempalte filters

--- traq/templatetags/addcss.py
+++ traq/templatetags/addcss.py
@@ -1,7 +1,22 @@
 #thanks to https://github.com/iivvoo
 from django import template
+from django.utils.safestring import mark_safe
+
 register = template.Library()
 
 @register.filter(name='addcss')
-def addcss(field, css):
+def addclass(field, css):
     return field.as_widget(attrs={"class":css})
+
+@register.filter(name='addattr')
+def addattr(field, args):
+    if args is None:
+        return false
+    arg_list=[arg.strip() for arg in args.split(',')]
+    return field.as_widget(attrs={arg_list[0]:arg_list[1]})
+
+@register.filter(name='bs_group')
+def bootstrap(field, column):
+    '''returns bootsrap form-group markup with a given column width. 
+       usage: {{ form.field | bs_group:'4' }}'''
+    return mark_safe('<div class="form-group col-md-%s"><label>%s</label>%s</div>' % (column, field.label, field))
