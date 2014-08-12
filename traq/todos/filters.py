63848ad31e58c929d05c80278bcb7c131d5fccbc
Brandon Conway <conwayb@pdx.edu>
2014-08-12 12:40:53 -0700

    further enhancements to scrum workflow pages

--- traq/todos/filters.py
+++ traq/todos/filters.py
@@ -9,6 +9,13 @@ from traq.tickets.filters import range_from_today, StartDateRangeFilter, TicketF
 import datetime
 from django.shortcuts import get_object_or_404
 
+class CustomBooleanFilter(django_filters.BooleanFilter):
+    def filter(self, qs, value):
+        if value is not None:
+            qs = qs.exclude(**{'%s__%s' % (self.name, self.lookup_type): value})
+            return qs
+        return qs
+
 class ToDoFilterSet(TicketFilterSet):
     def __init__(self, *args, **kwargs):
             super(ToDoFilterSet, self).__init__(*args, **kwargs)
@@ -38,6 +45,8 @@ class ToDoPriorityFilterSet(ToDoFilterSet):
         fields = ('due_range','sprint_end')
 
 class BacklogFilterSet(ToDoPriorityFilterSet):
+    estimate = CustomBooleanFilter('estimate', label=_('Estimated'), lookup_type='isnull') 
+    
     def __init__(self, *args, **kwargs):
         
         project_id = kwargs.get('project_id')
@@ -45,17 +54,18 @@ class BacklogFilterSet(ToDoPriorityFilterSet):
         super(ToDoFilterSet, self).__init__(*args, **kwargs)
         extra ={'status':1}
         if not self.data:
-            qdict = QueryDict('sprint_end=%s' % project.backlog()) 
+            qdict = QueryDict('sprint_end=%s&estimate=%s' % (project.backlog(), False)) 
             print qdict
             self.data = qdict
         self.filters['sprint_end'].widget=forms.Select(choices = get_choices(self.Meta.model, project, extra)) 
-         
-        self.filters.pop('priority')
+        
         self.filters.pop('status')
+        self.filters.pop('priority')
         self.filters.pop('due_range')
         self.filters.pop('assigned_to')
 
     class Meta:
         model = ToDo
-        fields = ('due_range','sprint_end')
+        fields = ('due_range','sprint_end', 'estimate')
+
 
