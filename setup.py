221d455b30cb5ae7f62fba67c615b8e05db0bdc9
Wyatt Baldwin <wyatt.baldwin@pdx.edu>
2014-10-29 09:51:20 -0700

    Add setup.py
    Move dependencies from requirements.txt to setup.py.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='traq',
+    version='0.0.0.dev0',
+    description='Traq',
+    long_description='Internal ticketing and invoice system for ARC',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django==1.5.6',
+        'djangocas',
+        'django-cloak',
+        'django-extensions',
+        'django_filter',
+        'Markdown==2.3.1',
+        'MySQL-python==1.2.5',
+        'python-ldap==2.3.13',
+        'pytz>=2013b',
+        'shortuuid',
+        'South<2.0',
+        'wsgiref==0.1.2',
+    ],
+)
