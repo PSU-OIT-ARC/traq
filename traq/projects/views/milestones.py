11cc23cfe89df800de2337712a636f5c3995b9e1
Wyatt Baldwin <wyatt.baldwin@pdx.edu>
2014-11-21 14:23:30 -0800

    PEP8 cleanup

--- traq/projects/views/milestones.py
+++ traq/projects/views/milestones.py
@@ -1,12 +1,11 @@
-import json
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.db import connection
-from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
 from ..forms import MilestoneForm
 from ..models import Project, Milestone
+
 
 @permission_required('projects.add_milestone')
 def create(request, project_id):
@@ -39,7 +38,8 @@ def edit(request, milestone_id):
     milestone = get_object_or_404(Milestone, pk=milestone_id)
     project = milestone.project
     if request.method == "POST":
-        form = MilestoneForm(request.POST, instance=milestone, project=project, created_by=milestone.created_by)
+        form = MilestoneForm(
+            request.POST, instance=milestone, project=project, created_by=milestone.created_by)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('milestones-detail', args=(project.pk,)))
