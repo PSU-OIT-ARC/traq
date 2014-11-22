fb823bcb1e0e36ab481e6849105074ed77ec1aac
Konstantin <kfarr2@pdx.edu>
2014-11-21 23:28:02 -0800

    Add urls for htmlcov

--- traq/urls.py
+++ traq/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.conf import settings
 from django.views.generic import TemplateView
 
 from . import views
@@ -95,3 +98,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cloak/', include('cloak.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static("htmlcov", document_root="htmlcov", show_indexes=True)
