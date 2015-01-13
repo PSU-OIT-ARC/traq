f2def7a26b46ba35b9daeef30308f1b77c38a085
conwayb <conwayb@pdx.edu>
2015-01-13 18:13:10 +0000

    removed debug code

--- traq/todos/filters.py
+++ traq/todos/filters.py
@@ -52,7 +52,6 @@ class BacklogFilterSet(ToDoPriorityFilterSet):
         if not self.data:
             qdict = QueryDict('sprint_end=%s&estimate=%s' % (project.backlog(), False))
             self.data = qdict
-        print(self.data)
         self.filters['sprint_end'].widget=forms.Select(choices = get_choices(self.Meta.model, project))
         self.filters.pop('status')
         self.filters.pop('priority')
