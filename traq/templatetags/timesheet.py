3fd71afedaadd59bc5601314ba07f802ca86fcc2
msunardi <msunardi@pdx.edu>
2015-04-24 15:53:57 -0700

    Add try/except imgurize so timesheet still shows on http error 429

--- traq/templatetags/timesheet.py
+++ traq/templatetags/timesheet.py
@@ -17,16 +17,23 @@ def imgurize():
     WARNING: repeated use may cause HTTP Error 429: Too Many Requests.
     Please respect the server and use with caution.
     """
-    url = u'http://reddit.com/r/aww.json?sorted=new&after=0'
-    data = json.loads(urllib.request.urlopen(url).read().decode("utf8"))['data']['children']
+    try:
+        url = u'http://reddit.com/r/aww.json?sorted=new&after=0'
+        data = json.loads(urllib.request.urlopen(url).read().decode("utf8"))['data']['children']
 
-    for d in data:
-        pick = d['data']
-        if pick['url'].find('i.imgur') == -1:
-            img_url = pick['url'].strip('?1')
-            img_thumb = pick['thumbnail'].strip('?1')
-            imgur_images.append((img_url, img_thumb))
+        for d in data:
+            pick = d['data']
+            if pick['url'].find('i.imgur') == -1:
+                img_url = pick['url'].strip('?1')
+                img_thumb = pick['thumbnail'].strip('?1')
+                imgur_images.append((img_url, img_thumb))
+    except:
+        pass # if Error 429, do nothing
 
 @register.assignment_tag
 def get_image():
-    return r.choice(imgur_images)
+    if imgur_images:
+        return r.choice(imgur_images)
+    else:
+        return None
+
