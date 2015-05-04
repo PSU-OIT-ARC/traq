f1c53c5efc0c9c679f8677bbef3bf1dc6780d5b6
conwayb <conwayb.pdx.edu>
2015-05-04 19:17:53 +0000

    Changes to Backlog scrum view filters and templates

--- traq/projects/views/scrum.py
+++ traq/projects/views/scrum.py
@@ -82,7 +82,7 @@ def backlog(request, project_id):
 def sprint_planning(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     #open or in progress todos not completed but with an estimate
-    todo_filterset = ToDoPriorityFilterSet(request.GET, queryset=ToDo.objects.filter(project=project, is_deleted=False, estimate__gte=0).exclude(status_id__in=[4,5]).order_by('status','rank', 'due_on'), project_id=project_id)
+    todo_filterset = ToDoPriorityFilterSet(request.GET, queryset=ToDo.objects.filter(project=project, is_deleted=False, estimate__gte=0).exclude(status_id__in=[4,5]).order_by('rank', 'due_on'), project_id=project_id)
     todos = todo_filterset
     q = request.GET.get('q', None)
     if q is not None:
